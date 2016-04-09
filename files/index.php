12a76f2d77c3c2158f7aecfb6fb5e2348ccfd786
Jonathan Berger <john@3kd.be>
2016-04-09 22:17:57 +0200

    bugfix error log on 404

--- files/index.php
+++ files/index.php
@@ -3,7 +3,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 require __DIR__.'/../app/AppKernel.php';
 
-$kernel = new AppKernel('prod', true);
+$kernel = new AppKernel('prod', false);
 //$kernel = new AppKernel('dev', true); // To activate debug tools
 $request = Request::createFromGlobals();
 $response = $kernel->handle($request);
